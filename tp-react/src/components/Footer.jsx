d189d9c3bc5c73df0865e5d9cfc1111c506aee02
caterina-tais <caterinadefranchis@epet12smandes.edu.ar>
2024-11-15 02:26:02 -0300

    tpfinal

---
+++ tp-react/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css'; 
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2024 Adivinanzas App. Todos los derechos reservados.</p>
+      <div className="social-links">
+        <a href="#X">X</a>
+        <a href="#instagram">Instagram</a>
+        <a href="#Tiktok">Tiktok</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
